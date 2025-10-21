81e2e618f01586959b584a1e53cde0e3290cc08d
Nils Lutz <info@nilslutz.de>
2025-10-21 21:22:10 +0200

    chore: update dependencies and add WebdriverIO configuration
    - Added new dependencies for WebdriverIO: @wdio/cli, @wdio/local-runner, @wdio/mocha-framework, @wdio/spec-reporter, wdio-timeline-reporter, and wdio-ui5-service.
    - Updated @openui5/types and @sapui5/ts-types to the latest versions.
    - Updated typescript to version 5.9.3.
    - Modified test script to specify the test directory.
    - Added wdi5 script for running WebdriverIO tests.

---
+++ app/timetable/webapp/test/wdi5/specs/timetable.test.ts
@@ -0,0 +1,22 @@
+import { wdi5 } from 'wdio-ui5-service';
+import {} from '../pageobjects/timeTableListReport.page';
+
+describe('Manage Time Entries', () => {
+  it('should log', () => {
+    const logger = wdi5.getLogger();
+    logger.log('hello world!');
+  });
+
+  // intentionally skipping this as you have to adjust things to your UI5 app :)
+  it.skip('should retrieve a UI5 control', async () => {
+    const appLocator = {
+      selector: {
+        controlType: 'sap.m.App',
+        viewName: 'ui5.typescript.helloworld.view.App',
+      },
+    };
+
+    const app = await browser.asControl(appLocator);
+    expect(app).toBeDefined();
+  });
+});
