cfc171456c38eec7c07dddbc2aa40dcd95561112
Nils Lutz <info@nilslutz.de>
2025-10-27 23:00:54 +0100

    feat: Update ESLint configuration to include additional ignore patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
 import cdsPlugin from '@sap/eslint-plugin-cds';
 import cds from '@sap/cds/eslint.config.mjs';
-export default [cdsPlugin.configs.recommended, ...cds.recommended];
+export default [
+  cdsPlugin.configs.recommended,
+  ...cds.recommended,
+  {
+    ignores: ['**/gen/**', '**/node_modules/**', '**/dist/**', '**/coverage/**', '**/*.config.mjs'],
+  },
+];
